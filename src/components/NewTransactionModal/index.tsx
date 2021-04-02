eccc26e89596a96d53e868d500d210dbf1b16b04
Marcelo Teixeira Cortes <marceloteixeiracortes@gmail.com>
2021-04-02 20:54:31 -0300

    Estrutura do formulário

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,7 @@
 import Modal from 'react-modal';
 
+import {Container} from './styles';
+
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: ()=> void;
@@ -8,9 +10,31 @@ interface NewTransactionModalProps {
 export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionModalProps) {
     return (
         <Modal 
-        isOpen={isOpen} 
-        onRequestClose={onRequestClose}
-      ><h2>Cadastrar transação</h2>
-      </Modal>
+            isOpen={isOpen} 
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+        >
+            <Container>
+                <h2>Cadastrar transação</h2>
+
+                <input 
+                    placeholder="Título"
+                />
+
+                <input 
+                    type="number"
+                    placeholder="Valor"
+                />
+
+                <input 
+                    placeholder="Categoria"
+                />
+
+                <button type="button">
+                    Cadastrar
+                </button>
+            </Container>   
+        </Modal>
     );
 }
